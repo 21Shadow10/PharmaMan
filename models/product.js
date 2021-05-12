088d04fe5f79b1f96e7595e16985b7f6830037ed
AT-LEGEND <atlegend0@gmail.com>
2021-05-12 11:55:26 +0530

    slight changes

---
+++ models/product.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    src: { type: String, required: true },
+    name: { type: String, required: true },
+    price: {
+        type: float,
+        required: true,
+    },
+    rating: {
+        type: number,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
